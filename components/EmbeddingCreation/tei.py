77ec58b9b8c49bbaf7dd9007b5b5c514aaf47ca7
bit2424 <nelsonquinones2424@gmail.com>
2024-10-22 08:35:57 +0000

    :bug: Fixed docker-compose build and connection issues

--- components/EmbeddingCreation/tei.py
+++ components/EmbeddingCreation/tei.py
@@ -2,6 +2,6 @@ from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
 
 class textEmbeddingsInterference:
     def compute_embedding(doc: str):
-        embeddings = HuggingFaceEndpointEmbeddings(model="http://localhost:8080")
+        embeddings = HuggingFaceEndpointEmbeddings(model="http://tei_container:80")
         query_result = embeddings.embed_query(doc)
         return query_result
